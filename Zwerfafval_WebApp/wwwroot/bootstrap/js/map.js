5a17486449f3bf69d30354fd92719ca74631da34
JurPurr <j.kempf@student.avans.nl>
2025-06-08 12:22:45 +0200

    Attempt to create a heatmap using Leaflet.Heat

---
+++ Zwerfafval_WebApp/wwwroot/bootstrap/js/map.js
@@ -0,0 +1,6 @@
+﻿window.initMap = () => {
+    var map = L.map('map').setView([51.571915, 4.768323], 13); // Breda als startpunt
+    L.tileLayer('https://api.maptiler.com/maps/nl-cartiqo-topo/{z}/{x}/{y}.png?key=mvRA2CGG1T8gvSWGDugz', {
+        attribution: '<a href="https://cartiqo.nl/" target="_blank">© Cartiqo</a> <a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>',
+    }).addTo(map);
+}
