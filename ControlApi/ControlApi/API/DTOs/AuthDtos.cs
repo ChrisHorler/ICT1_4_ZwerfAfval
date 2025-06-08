4cdf4f8c5ae2602ef2581808422b733c8629effb
ChrisHorler <csh.sammt@gmail.com>
2025-06-08 14:38:20 +0200

    Updated AuthDTO
    - Updated Authentication DTO to handle Authentication rules
    - Updated Authentication Unit Tests to use Register and Login Class instead of Record

--- ControlApi/ControlApi/API/DTOs/AuthDtos.cs
+++ ControlApi/ControlApi/API/DTOs/AuthDtos.cs
@@ -2,12 +2,27 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ControlApi.API.DTOs;
 
-public record RegisterRequest(
-    [property: Required, EmailAddress] string email,
-    [property: Required, MinLength(8),
-               RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).+$",
-                   ErrorMessage = "Password must contain an uppercase letter, lowercase letter, number, and symbol.")]
-    string password
-);
-public record LoginRequest(string email, string password);
+public class RegisterRequest
+{
+    [Required]
+    [EmailAddress]
+    public string email { get; init; } = string.Empty;
+
+    [Required]
+    [MinLength(8)]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$",
+        ErrorMessage = "Invalid E-mail or Password")]
+    public string password { get; init; } = string.Empty;
+}
+
+public class LoginRequest
+{
+    [Required]
+    [EmailAddress]
+    public string email { get; init; } = string.Empty;
+
+    [Required]
+    [MinLength(8)]
+    public string password { get; init; } = string.Empty;   
+}
 public record AuthResponse(string token, int userId,string email);
