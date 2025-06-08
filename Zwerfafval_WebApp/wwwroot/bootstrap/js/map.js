548c106c695e4bc81ae020442d0841e19639bac3
JurPurr <j.kempf@student.avans.nl>
2025-06-08 15:04:04 +0200

    Tried to fix the heatmap
    Changed Overview.Razor and Mainlayout.razor, but it still doesn't work.

--- Zwerfafval_WebApp/wwwroot/bootstrap/js/map.js
+++ Zwerfafval_WebApp/wwwroot/bootstrap/js/map.js
@@ -1,5 +1,5 @@
 ﻿window.initMap = () => {
-    var map = L.map('map').setView([51.571915, 4.768323], 13); // Breda als startpunt
+    var map = L.map('map').setView([51.571915, 4.768323], 13); // Breda
     L.tileLayer('https://api.maptiler.com/maps/nl-cartiqo-topo/{z}/{x}/{y}.png?key=mvRA2CGG1T8gvSWGDugz', {
         attribution: '<a href="https://cartiqo.nl/" target="_blank">© Cartiqo</a> <a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>',
     }).addTo(map);
