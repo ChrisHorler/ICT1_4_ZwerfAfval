6b762787447668463709b8ba5dc67160c3a72d0a
OldMartijntje_ubuntu <oldmartijntje@gmail.com>
2025-06-12 17:08:33 +0200

    Sketch

--- ControlApi/ControlApi/API/Models/SensoringConnector.cs
+++ ControlApi/ControlApi/API/Models/SensoringConnector.cs
@@ -1,6 +1,7 @@
 using ControlApi.API.DTOs;
 using ControlApi.API.Services;
 using ControlApi.Data;
+using ControlApi.Data.Entities;
 using Newtonsoft.Json;
 
 namespace ControlApi.SensoringConnection.Models;
@@ -32,14 +33,23 @@ public class SensoringConnector
         this._logger.LogInformation("DbContext Provided to SensoringConnector");
         this._db = db;
     }
-    
+
     public async Task PullAsync(CancellationToken cancellationToken)
     {
         if (this._db == null)
         {
             this._logger.LogInformation("Tried to pull data from Sensoring API without DbContext.");
         }
-        this._logger.LogInformation("Updating our Trash Collection with the Sensoring API.");
+
+        var latestItem = this._db.detections
+            .OrderByDescending(e => e.timeStamp)
+            .FirstOrDefault();
+        if (latestItem == null)
+        {
+            latestItem = new Detection();
+            latestItem.timeStamp = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        }
+        this._logger.LogInformation("Updating our Trash Collection with the Sensoring API.\nLastUpdate: {item}", latestItem.timeStamp);
         var client = _httpClientFactory.CreateClient();
         
         var response = await client.GetAsync(this._apiUrl, cancellationToken);
