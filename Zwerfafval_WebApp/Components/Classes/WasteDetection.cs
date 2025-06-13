7843b905de7120d56f04c033986c88dadcc099b4
JurPurr <j.kempf@student.avans.nl>
2025-06-13 14:16:48 +0200

    Preperations for data from API

---
+++ Zwerfafval_WebApp/Components/Classes/WasteDetection.cs
@@ -0,0 +1,10 @@
+﻿namespace Zwerfafval_WebApp.Components.Classes
+{
+    public class WasteDetection
+    {
+        public DateTime Date { get; set; }
+        public int Detection {  get; set; }
+        public string Category { get; set; }
+
+    }
+}
