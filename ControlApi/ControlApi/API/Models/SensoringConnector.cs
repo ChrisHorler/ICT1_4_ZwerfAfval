fc4a2c3d53e808fd9883e24651c2e36381d4c084
oldmartijntje_TH <oldmartijntje@gmail.com>
2025-06-18 16:27:35 +0200

    add time to poidetection

--- ControlApi/ControlApi/API/Models/SensoringConnector.cs
+++ ControlApi/ControlApi/API/Models/SensoringConnector.cs
@@ -95,7 +95,8 @@ public class SensoringConnector
                             det.detectionPOIs.Add(new DetectionPOI()
                             {
                                 POIID = poiObj.POIID,
-                                detectionRadiusM = DETECTION_RADIUS
+                                detectionRadiusM = DETECTION_RADIUS,
+                                timeStamp = det.timeStamp
                             });
                         }
                         trashDets.Add(det);
