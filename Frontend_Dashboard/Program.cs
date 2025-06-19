ba302da00b2b103a4b000b5a76a2eb0505685452
whoskhoi <khoivaniersel@gmail.com>
2025-06-19 18:43:17 +0200

    Added more old code
    In program.cs and from the graph and barchart

--- Frontend_Dashboard/Program.cs
+++ Frontend_Dashboard/Program.cs
@@ -1,10 +1,23 @@
 using Frontend_Dashboard.Components;
+using Frontend_Dashboard.Components.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddSingleton<DateService>();
+builder.Services.AddBlazorBootstrap();
+builder.Services.AddScoped<IDetectionDataService, DetectionDataService>();
+builder.Services.AddHttpClient("BackendApi", client =>
+{
+    var baseUrl = builder.Configuration["API_BASE_URL"];
+    if (string.IsNullOrEmpty(baseUrl))
+        throw new Exception("API_BASE_URL enviromental variable not detected");
+
+    client.BaseAddress = new Uri(baseUrl);
+});
 
 var app = builder.Build();
 
