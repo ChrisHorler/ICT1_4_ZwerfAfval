3810520d4e0999ec2d71e7375719c3948d5509b7
whoskhoi <khoivaniersel@gmail.com>
2025-06-19 18:37:06 +0200

    Added files from the old code
    Added services and models from previous frontend code.

---
+++ Frontend_Dashboard/Components/Services/DetectionDataService.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using Frontend_Dashboard.Components.Models;
+
+namespace Frontend_Dashboard.Components.Services
+{
+    public class DetectionDataService : IDetectionDataService
+    {
+        private readonly HttpClient _httpClient;
+
+        public DetectionDataService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<DetectionData>> GetDetectionDataAsync()
+        {
+            try
+            {
+                // Api endpoint naam hieronder veranderen wanneer deze bekend is 
+                var response = await _httpClient.GetFromJsonAsync<List<DetectionData>>("control-api/collection");
+                return response ?? new List<DetectionData>();
+            }
+            catch (Exception ex)
+            {
+                Console.Write($"[DetectionDataService] Error while fetching data");
+                return new List<DetectionData>();
+            }
+        }
+    }
+}
