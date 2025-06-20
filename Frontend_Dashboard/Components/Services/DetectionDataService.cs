9552947526866b17837f3eaef0ecee04a2998b91
oldmartijntje_TH <oldmartijntje@gmail.com>
2025-06-20 21:01:59 +0200

    Reach the api touchdown

--- Frontend_Dashboard/Components/Services/DetectionDataService.cs
+++ Frontend_Dashboard/Components/Services/DetectionDataService.cs
@@ -7,10 +7,16 @@ namespace Frontend_Dashboard.Components.Services
     public class DetectionDataService : IDetectionDataService
     {
         private readonly HttpClient _httpClient;
+        private readonly IConfiguration _config;
+        private readonly string _apiUrl;
 
-        public DetectionDataService(HttpClient httpClient)
+        public DetectionDataService(HttpClient httpClient, IConfiguration config)
         {
             _httpClient = httpClient;
+            _config = config;
+            _apiUrl = config["BackendAPI:BaseUrl"] 
+                             ?? Environment.GetEnvironmentVariable("BackendAPI:BaseUrl") 
+                             ?? throw new InvalidOperationException("'BackendAPI:BaseUrl' not found");
         }
 
         public async Task<List<DetectionData>> GetDetectionDataAsync()
@@ -18,7 +24,8 @@ namespace Frontend_Dashboard.Components.Services
             try
             {
                 // Api endpoint naam hieronder veranderen wanneer deze bekend is 
-                var response = await _httpClient.GetFromJsonAsync<List<DetectionData>>("control-api/collection");
+                var response = await _httpClient.GetFromJsonAsync<List<DetectionData>>($"{this._apiUrl}api/Detections/barchart?date=2025-06-20");
+                Console.WriteLine(response);
                 return response ?? new List<DetectionData>();
             }
             catch (Exception)
